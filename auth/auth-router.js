c7e02a2d023e7e2bd545b75f9a865cc91cbd3b00
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-02-23 18:31:19 -0800

    Initialize project
    Create database for testing
    
    Add directory to hold JWT secret & include in gitignore
    
    Update knexfile.js to include testing directories
    
    Add dependencies: JWT, bcrypt, morgan
    
    Update dependencies

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Import secret to use for JWT
+const secret = require('../config/secrets')
+// User must create a config folder containing a file 
+// secrets.js must contain an object with the secret key titled jwtSecret
 
 router.post('/register', (req, res) => {
   // implement registration
